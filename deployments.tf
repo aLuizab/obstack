041aca3805648936071540244d11cb1b3e20559d
Ana Luiza Botelho <aluiza.primo@gmail.com>
2024-07-13 14:36:33 -0300

    feat: grafana setup

---
+++ deployments.tf
@@ -0,0 +1,146 @@
+resource "kubernetes_namespace" "kube-namespace" {
+  metadata {
+    name = "prometheus"
+  }
+}
+
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  namespace  = kubernetes_namespace.kube-namespace.id
+  create_namespace = true
+  version    = "45.7.1"
+  values = [
+    file("./prometheus/values.yaml")
+  ]
+  timeout = 2000
+  
+
+set {
+    name  = "podSecurityPolicy.enabled"
+    value = true
+  }
+
+  set {
+    name  = "server.persistentVolume.enabled"
+    value = false
+  }
+
+  set {
+    name  = "grafana.enabled"
+    value = false
+  }
+
+  set {
+    name = "server\\.resources"
+    value = yamlencode({
+      limits = {
+        cpu    = "200m"
+        memory = "50Mi"
+      }
+      requests = {
+        cpu    = "100m"
+        memory = "30Mi"
+      }
+    })
+  }
+}
+
+resource "kubernetes_namespace" "grafana" {
+  metadata {
+    name = "grafana"
+  }
+}
+
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "grafana"
+  namespace  = kubernetes_namespace.grafana.id
+  create_namespace = true
+  version    = "7.0.0"
+  values = [
+    file("./grafana/values.yaml")
+  ]
+  timeout = 2000
+  
+
+set {
+    name  = "podSecurityPolicy.enabled"
+    value = true
+  }
+
+  set {
+    name  = "server.persistentVolume.enabled"
+    value = false
+  }
+
+  set {
+    name = "server\\.resources"
+    value = yamlencode({
+      limits = {
+        cpu    = "200m"
+        memory = "50Mi"
+      }
+      requests = {
+        cpu    = "100m"
+        memory = "30Mi"
+      }
+    })
+  }
+}
+
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "k8s-ns-by-tf"
+  }
+}
+
+resource "kubernetes_deployment" "nginx-exemple" {
+  metadata {
+    name = "nginx-example"
+    labels = {
+      test = "MyExampleApp"
+    }
+    namespace = "k8s-ns-by-tf"
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        test = "Nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          test = "Nginx"
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx:1.21.6"
+          name  = "nginx"
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+
